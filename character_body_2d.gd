96e4c10efff24ec071823ee92e5912a9ee718e45
Wu-Zetian <azkandarmana@gmail.com>
2026-08-14 19:25:24 +0700

    movement-update

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,27 +1,37 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
+const MAX_SPEED = 3000.0
+const ACCELERATION = 2000.0
+const FRICTION = 4000.0
+var target_xvelocity = 0.0
+var target_yvelocity = 0.0
 
 func _physics_process(delta: float) -> void:
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	#var direction := Input.get_axis("ui_left", "ui_right")
+	#if direction:
+		#target_xvelocity = direction * MAX_SPEED
+		#velocity.x = move_toward(velocity.x, target_xvelocity, ACCELERATION * delta)
+	#else:
+		#velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
 		
-	var directionUp := Input.get_axis("ui_up", "ui_down")
-	if directionUp:
-		velocity.y = directionUp * SPEED
+	#var directionUp := Input.get_axis("ui_up", "ui_down")
+	#if directionUp:
+		#target_yvelocity = directionUp * MAX_SPEED
+		#velocity.y = move_toward(velocity.y, target_yvelocity, ACCELERATION * delta)
+	#else:
+		#velocity.y = move_toward(velocity.y, 0, FRICTION * delta)
+		#velocity.y = directionUp * SPEED
+	#else:
+		#velocity.y = move_toward(velocity.y, 0, SPEED)
+	
+	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")	
+	if direction:
+		var target_velocity := direction * MAX_SPEED
+		velocity = velocity.move_toward(target_velocity, ACCELERATION * delta)
 	else:
-		velocity.y = move_toward(velocity.y, 0, SPEED)
-
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+		
 	move_and_slide()
