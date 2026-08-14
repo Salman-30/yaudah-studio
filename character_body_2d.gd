6ebc92f5e19c2bc7c0c287b65aa88097c2fee6a5
Alpinnn3 <alvinahmadfardhani3@gmail.com>
2026-08-14 20:11:38 +0700

    Tes mekanik dash

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,14 +1,12 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+var SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+const DASH_RANGE = 10000
 
 
 func _physics_process(delta: float) -> void:
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -23,5 +21,10 @@ func _physics_process(delta: float) -> void:
 		velocity.y = directionUp * SPEED
 	else:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
-
+	
+	if Input.is_action_just_pressed("ui_accept"):
+		SPEED = DASH_RANGE
+	else:
+		SPEED = 300
+		
 	move_and_slide()
