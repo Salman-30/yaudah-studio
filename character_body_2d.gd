8e24f0ed5396185370c9bb869c711aa78eceb672
Wu-Zetian <azkandarmana@gmail.com>
2026-08-15 17:24:51 +0700

    add enemies and dash

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,11 +1,18 @@
 extends CharacterBody2D
+@onready var dash_cooldown = $dash_cooldown
 
+const DASH_RANGE = 500.0
 const SPEED = 300.0
-const MAX_SPEED = 3000.0
+const MAX_SPEED = 1000.0
 const ACCELERATION = 2000.0
-const FRICTION = 4000.0
+const FRICTION = 6000.0
 var target_xvelocity = 0.0
 var target_yvelocity = 0.0
+var dash_capacity = true
+var last_direction := Vector2.ZERO
+
+func _on_dash_cooldown_timeout() -> void:
+	dash_capacity = true
 
 func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
@@ -27,11 +34,24 @@ func _physics_process(delta: float) -> void:
 	#else:
 		#velocity.y = move_toward(velocity.y, 0, SPEED)
 	
-	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")	
+	var direction := Input.get_vector("key_a", "key_d", "key_w", "key_s")	
 	if direction:
 		var target_velocity := direction * MAX_SPEED
 		velocity = velocity.move_toward(target_velocity, ACCELERATION * delta)
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+	
+	if last_direction != direction and direction != Vector2.ZERO:
+		last_direction = direction
+	
+	var is_dashing = false
+	
+	if Input.is_action_just_pressed("ui_accept") == true and dash_capacity == true and is_dashing == false:
+		is_dashing = true
+		dash_cooldown.start()
+		dash_capacity = false
+		var dash := last_direction * DASH_RANGE
+		move_and_collide(dash)
 		
+			
 	move_and_slide()
---
+++ enemy.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+@onready var player: CharacterBody2D = $"../Player"
+const MAX_SPEED = 900.0
+const ACCELERATION = 3000.0
+const FRICTION = 6000.0
+var direction = Vector2.ZERO
+
+func _physics_process(delta: float) -> void:
+	if position.y < player.position.y:
+		direction = Vector2.DOWN
+		velocity = velocity.move_toward(MAX_SPEED * direction, ACCELERATION * delta)
+	elif position.y > player.position.y:
+		direction = Vector2.UP
+		velocity = velocity.move_toward(MAX_SPEED * direction, ACCELERATION * delta)
+
+	move_and_slide()
