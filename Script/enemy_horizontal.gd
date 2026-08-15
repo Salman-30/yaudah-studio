f0498291222265afed6489ef371fa47f1af2ef25
Wu-Zetian <azkandarmana@gmail.com>
2026-08-16 05:37:12 +0700

    enemy dash

--- Script/enemy_horizontal.gd
+++ Script/enemy_horizontal.gd
@@ -1,17 +1,33 @@
 extends CharacterBody2D
 
 @onready var player: CharacterBody2D = $"../Player"
+@onready var dash_cooldown = $dash_cooldown
 const MAX_SPEED = 900.0
 const ACCELERATION = 3000.0
 const FRICTION = 6000.0
 var direction = Vector2.ZERO
+var DASH_RANGE = 500.0
+var dash_capacity = true
+
+func _on_dash_cooldown_timeout() -> void:
+	dash_capacity = true
+
 
 func _physics_process(delta: float) -> void:
 	if position.x < player.position.x:
 		direction = Vector2.RIGHT
 		velocity = velocity.move_toward(MAX_SPEED * direction, ACCELERATION * delta)
-	elif position.x > player.position.x:
+	if position.x > player.position.x:
 		direction = Vector2.LEFT
 		velocity = velocity.move_toward(MAX_SPEED * direction, ACCELERATION * delta)
+		
+	if position.x + 500 < player.position.x and dash_capacity == true:
+		dash_capacity = false
+		dash_cooldown.start()
+		move_and_collide(DASH_RANGE * Vector2.RIGHT)
+	if position.x - 500 > player.position.x and dash_capacity == true:
+		dash_capacity = false
+		dash_cooldown.start()
+		move_and_collide(DASH_RANGE * Vector2.LEFT)	
 
 	move_and_slide()
--- Script/enemy_vertical.gd
+++ Script/enemy_vertical.gd
@@ -1,10 +1,16 @@
 extends CharacterBody2D
 
 @onready var player: CharacterBody2D = $"../Player"
+@onready var dash_cooldown = $dash_cooldown
 const MAX_SPEED = 900.0
 const ACCELERATION = 3000.0
 const FRICTION = 6000.0
 var direction = Vector2.ZERO
+var DASH_RANGE = 500.0
+var dash_capacity = true
+
+func _on_dash_cooldown_timeout() -> void:
+	dash_capacity = true
 
 func _physics_process(delta: float) -> void:
 	if position.y < player.position.y:
@@ -14,6 +20,14 @@ func _physics_process(delta: float) -> void:
 		direction = Vector2.UP
 		velocity = velocity.move_toward(MAX_SPEED * direction, ACCELERATION * delta)
 
+	if position.y + 500 < player.position.y and dash_capacity == true:
+		dash_capacity = false
+		dash_cooldown.start()
+		move_and_collide(DASH_RANGE * Vector2.DOWN)
+	if position.y - 500 > player.position.y and dash_capacity == true:
+		dash_capacity = false
+		dash_cooldown.start()
+		move_and_collide(DASH_RANGE * Vector2.UP)	
 	move_and_slide()
 
 
